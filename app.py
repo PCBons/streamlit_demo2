7494179c214b59138b646ce5a82fa68efc625bfd
PCBons <43748923+PCBons@users.noreply.github.com>
2025-05-21 13:30:48 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+gokken = []
+st.title('Welkom bij de snoeppot app')
+
+@st.fragment
+def doe_gok():
+
+	with st.form(key = '1'):
+		gok = st.number_input('Vul hier je gok in:')
+		st.form_submit_button()
+	gokken.append(gok)
+
+	st.write(gokken[1:])
+	st.write(f'Er hebben tot nu toe {len(gokken)-1} mensen meegedaan.')
+
+doe_gok()
